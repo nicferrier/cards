057424b379c1deea7b52f449df828f149b942898
Nic Ferrier <nicholas.ferrier@bwinparty.com>
2014-07-04 16:21:47 +0100

    add commented out section about how to js add a card

--- scripts/example.js
+++ scripts/example.js
@@ -1,2 +1,6 @@
 var bpg_cards = require ("./bpg-cards.js");
 var PlayingCard = bpg_cards.card_init(document);
+
+//var elem = new PlayingCard();
+//elem.setAttribute("")
+//document.body.appendChild(elem);
