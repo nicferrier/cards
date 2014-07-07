e089b7490e6485b371b14ebb344f174fb88a875c
Nic Ferrier <nicholas.ferrier@bwinparty.com>
2014-07-07 19:22:27 +0100

    move the css into the js

--- scripts/bpg-cards.js
+++ scripts/bpg-cards.js
@@ -1,13 +1,101 @@
+/*
+ * BwinParty Playing Cards
+ * 
+ * Copyright (C) 2014 by bwinparty
+ * 
+ * This is an HTML5 WebComponent element for playing cards.
+ * 
+ * The styles required for the WebComponent are embedded in the
+ * document by this library.
+ */
+
 var Platform = require("polyfill-webcomponents");
 var util = require("util");
+
+var cssText = ".card {\
+    background-image: url('/img/cardback.gif');\
+    border-color: #808080 #000000 #000000 #808080;\
+    border-width: 1px;\
+    border-style: solid;\
+    color: #000000;\
+    font-size: 20pt;\
+    position: absolute;\
+    width:  3.75em;\
+    height: 5.00em;\
+}\
+\
+.front {\
+    background-color: #ffffff;\
+    color: #000000;\
+    position: absolute;\
+    width: 100%;\
+    height: 100%;\
+}\
+\
+.face {\
+    border: 1px solid #000000;\
+    position: absolute;\
+    left: 0.60em;\
+    top:  0.45em;\
+    width:  2.6em;\
+    height: 4.0em;\
+}\
+\
+.red { \
+    color: #ff0000; \
+}\
+\
+.index {\
+    font-size: 50%;\
+    font-weight: bold;\
+    text-align: center;\
+    position: absolute;\
+    left: 0.25em;\
+    top:  0.25em;\
+}\
+\
+.ace {\
+    font-size: 300%;\
+    position: absolute;\
+    left: 0.225em;\
+    top:  0.250em;\
+}\
+\
+.spotA1 { position: absolute; left: 0.60em; top: 0.5em; }\
+.spotA2 { position: absolute; left: 0.60em; top: 1.5em; }\
+.spotA3 { position: absolute; left: 0.60em; top: 2.0em; }\
+.spotA4 { position: absolute; left: 0.60em; top: 2.5em; }\
+.spotA5 { position: absolute; left: 0.60em; top: 3.5em; }\
+.spotB1 { position: absolute; left: 1.55em; top: 0.5em; }\
+.spotB2 { position: absolute; left: 1.55em; top: 1.0em; }\
+.spotB3 { position: absolute; left: 1.55em; top: 2.0em; }\
+.spotB4 { position: absolute; left: 1.55em; top: 3.0em; }\
+.spotB5 { position: absolute; left: 1.55em; top: 3.5em; }\
+.spotC1 { position: absolute; left: 2.50em; top: 0.5em; }\
+.spotC2 { position: absolute; left: 2.50em; top: 1.5em; }\
+.spotC3 { position: absolute; left: 2.50em; top: 2.0em; }\
+.spotC4 { position: absolute; left: 2.50em; top: 2.5em; }\
+.spotC5 { position: absolute; left: 2.50em; top: 3.5em; }\
+";
 
 // A JS example of using this...
 //  var elem = new PlayingCard();
 //  document.body.appendChild(elem);
 
-
 var bpg_cards = 
   (function () {
+     // Setup the styles
+     var styleE = document.createElement("style");
+     styleE.type = "text/css";
+     if (styleE.styleSheet) {
+       styleE.styleSheet.cssText = cssText;
+     }
+     else {
+       styleE.appendChild(document.createTextNode(cssText));
+     }
+     (document.head || document.getElementsByTagName('head')[0]).appendChild(styleE);
+
+     // Setup the element
      var playingCardType = Object.create(HTMLElement.prototype);
      playingCardType.createdCallback = function ()  {
        // Capture 'this'
@@ -18,7 +106,6 @@ var bpg_cards =
          var val = self.getAttribute(name);
          return (val == null && defaultValue != null) ? defaultValue:val;
        };
-
 
        // We need to cope with nulls in these attributes
        if (this.getAttribute("suit") && this.getAttribute("value")) {
